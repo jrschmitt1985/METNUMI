518c901e66a1d15f05fa113bbd9e6e38672b2f42
jrschmitt1985 <jrschmitt1985@gmail.com>
2026-07-12 18:16:11 -0300

    push mac EXAME

--- EXAME/RF/lista1exame/ex6secanteModificadoExame.py
+++ EXAME/RF/lista1exame/ex6secanteModificadoExame.py
@@ -1,4 +1,23 @@
 import numpy as np
 
-def (fx):
+def f(x):
     return np.exp(-x)-x
+
+s = 0.01
+x = 1
+iteracoes = 0
+n=6
+epest = 100
+eppara = 0.5*10**(2-n)
+
+while epest >= eppara:
+    x_new = x-((s*x*f(x))/(f(x+(s*x))-f(x)))
+    
+    if iteracoes > 0:
+        epest = abs((x_new - x)/x_new)*100
+
+    
+    x = x_new
+    iteracoes += 1
+
+print(f"Raiz: {x:.6f} | f(x) = {f(x):.2e} | Iterações: {iteracoes:.6f} | Erro: {epest:.6f}%")
